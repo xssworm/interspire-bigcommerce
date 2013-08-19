ae2d0408a52948d660e8f093c638102adbf9be2c
katzwebdesign <katzwebdesign@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-19 22:00:41 +0000

    A big update.
    = 1.7 on 2013-08-19 =
    * Improved: Now uses a script that allows searching of products and categories to insert
    * Improved: Now you can watch the generation of the product and category list while it's processing. No more guessing whether it's working or not.
    * Fixed: Product links are now properly fetched from the `custom_url` parameter.
    * Fixed: Cache would always need to be rebuilt
    * Fixed: Only 50 categories were retrieved
    * Fixed: Child categories were not properly displayed
    
    git-svn-id: https://plugins.svn.wordpress.org/interspire-bigcommerce/tags/1.7@758783 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ lib/class.media.php
@@ -0,0 +1,142 @@
+<?php
+
+// Plugin Media Class
+class Bigcommerce_media {
+
+	// Tied To WP Hook By The Same Name - Adds Product Images Tab To Media Popup
+	function media_upload_tabs( $tabs ) {
+		return array_merge(
+			$tabs, array( 'wpinterspire' => __( 'Bigcommerce', 'wpinterspire' ) )
+		);
+	}
+
+	// Tied To WP Hook By The Same Name - Adds Menu Item For Processing Media
+	function media_upload_wpinterspire() {
+		return wp_iframe( array( 'Bigcommerce_media', 'media_process' ) );
+	}
+
+	// Tied To WP Hook By The Same Name - Ads Icon To WYSIWYG Posts/Pages Editor
+	function media_buttons_context( $context ) {
+		if( ! Bigcommerce_settings::$configured ) { return $context; }
+		return $context . '
+			<style>
+			.wp-media-buttons .wpinterspire { padding-left: 3px!important; }
+			.wp-media-buttons .wpinterspire span.wp-media-buttons-icon {
+				background: transparent url(' . plugins_url( 'favicon-small.png', BIGCOMMERCE_PLUGIN_FILE ) . ') left top no-repeat;
+				width: 12px;
+				height:17px;
+			}
+			</style>
+			<a href="#TB_inline?width=640&inlineId=interspire_select_product" class="thickbox button wpinterspire"
+				title="' . __( 'Add Bigcommerce Link', 'wpinterspire' ) . '">
+				<span class="wp-media-buttons-icon"></span>
+				'.__( 'Add Bigcommerce Link', 'wpinterspire' ).'</a>
+		';
+	}
+
+	// Is the current page a page where we want to load the media functions?
+	function is_bigcommerce_page() {
+		global $pagenow,$post;
+		return (
+			(in_array($pagenow, array('post.php', 'post-new.php')) && isset($post) && !empty($post->ID)) ||
+			($pagenow === 'options-general.php' && isset($_GET['page']) && $_GET['page'] === 'bigcommerce')
+		);
+	}
+	// Tied To WP Hook By The Same Name - Admin Area Footer
+	function admin_footer() {
+		if(!self::is_bigcommerce_page()) { return; }
+		$storepath = Bigcommerce_parser::storepath();
+		require( dirname( __FILE__ ) . '/../views/mce-popup.html.php' );
+	}
+
+	function print_scripts() {
+		global $pagenow,$post;
+
+		if(!self::is_bigcommerce_page()) { return; }
+		wp_enqueue_script('thickbox');
+		wp_enqueue_script( 'select2', plugins_url( 'select2/select2.min.js', BIGCOMMERCE_PLUGIN_FILE ), array('jquery'), false, true );
+		wp_enqueue_style( 'select2', plugins_url( 'select2/select2.css', BIGCOMMERCE_PLUGIN_FILE ));
+	}
+
+	// Presents Product Image Choices
+	function media_process() {
+
+		echo sprintf('<div class="updated" style="margin-top:20px" id="wpint_loading_images"><p>%s</p></div>', __('Loading images&hellip;', 'wpinterspire'));
+
+		$Products = Bigcommerce_api::GetProducts();
+
+		// Present Other Tabs
+		media_upload_header();
+
+		// Handle No Products
+		if( ! $Products ) {
+			echo '
+				<div class="tablenav">
+					<form id="filter">
+						<h3>'
+							. __( 'The Plugin settings have not been properly configured.', 'wpinterspire' ) .
+						'</h3>
+					</form>
+				</div>
+			';
+			return;
+		}
+
+		// Pagination Variables
+		$perpage = apply_filters('wpinterspire_images_per_page', 20);
+		$page = isset( $_GET['paged'] ) ? $_GET['paged'] : 1;
+	   	$start = $perpage * ( $page - 1 );
+	   	$end = $start + ( $perpage - 1 );
+		$total_images = 0;
+
+		$all_images = isset($_GET['bc-cache']) ? false : maybe_unserialize(get_option( 'wpinterspire_product_images'));
+
+		if(empty($all_images) || isset($_GET['bc-cache'])) {
+
+			// Loop Products
+			$all_images = array();
+
+			foreach( $Products as $product ) {
+
+				// Skip Products Without Images
+				$path = Bigcommerce_parser::GetImage( $product );
+
+				if(!$path) { continue; }
+
+				$all_images[(string)$product->id] = array(
+					'productid'	=> (string)$product->id,
+					'name' => (string)$product->name,
+					'path' => (string)$path,
+				);
+			}
+			delete_option( 'wpinterspire_product_images' );
+			add_option( 'wpinterspire_product_images', $all_images, '', 'no');
+		}
+
+		$total_images = sizeof($all_images);
+		$i = -1;
+		foreach($all_images as $image) {
+			$i++;
+
+			// Limit To Per Page Quantity
+			if( $i < $start || $i > $end ) { continue; }
+
+			$images[$image['productid']] = $image;
+		}
+
+		$paginate_links = str_replace(array('&amp;bc-cache=1', '&bc-cache=1'), '', paginate_links(
+			array(
+				'base' => add_query_arg( 'paged', '%#%' ),
+				'show_all' => true,
+				'format' => '',
+				'total' => ceil( $total_images / $perpage ),
+				'current' => $page,
+			)
+		));
+
+		// Output
+		require( dirname( __FILE__ ) . '/../views/media.html.php' );
+	}
+}
+
+?>
---
+++ lib/class.parser.php
@@ -0,0 +1,288 @@
+<?php
+
+// Plugin Parser Class
+class Bigcommerce_parser {
+
+	// Cleans Store URL
+	public function storepath( $api=false ) {
+    	$options = Bigcommerce_settings::get_options();
+
+		// Ensure API URL Is Secure
+		$options->storepath = str_replace( 'http://', 'https://', $options->storepath );
+
+		// Convert API v1 URLs To v2
+		$options->storepath = str_replace( '/xml.php', '/api/v2/', $options->storepath );
+
+		// Ensure The API URL Has a Trailing Slash
+		$options->storepath = trailingslashit( $options->storepath );
+
+		// Ensure The API URL Contains The API Path
+		$options->storepath = ( strstr( $options->storepath, '/api/v2/' ) )
+			? $options->storepath
+			: "{$options->storepath}api/v2/";
+
+		// Remove API Path, Except For The API
+		return ( $api )
+			? $options->storepath
+			: str_replace( '/api/v2/', '/', $options->storepath );
+	}
+
+	// Converts JSON To Object
+	public function JSONToObject( $json ) {
+
+		// Try To Convert
+		try {
+			$response = json_decode($json);
+			return $response;
+
+		// Catch Error In Conversion
+		} catch ( Exception $error ) {
+			Bigcommerce_settings::$errors[] = $error;
+			return false;
+		}
+
+		// Handle Bad Response
+		if( isset( $response->errors->error[0]->message ) ) {
+			Bigcommerce_settings::$errors[] = $response->errors->error[0]->message;
+			return false;
+		}
+
+		// Handle Good Response
+		return isset( $response->$test ) ? $response->$test : false;
+	}
+
+	// Converts XML To Object
+	public function XmlToObject( $xml, $test ) {
+
+		// Try To Convert
+		try {
+			$response = new SimpleXMLElement( $xml, LIBXML_NOCDATA );
+			return $response;
+
+		// Catch Error In Conversion
+		} catch ( Exception $error ) {
+			Bigcommerce_settings::$errors[] = $error;
+			return false;
+		}
+
+		// Handle Bad Response
+		if( isset( $response->errors->error[0]->message ) ) {
+			Bigcommerce_settings::$errors[] = $response->errors->error[0]->message;
+			return false;
+		}
+
+		// Handle Good Response
+		return isset( $response->$test ) ? $response->$test : false;
+	}
+
+	// Get an array of all image paths
+	public function GetImages( $link, $all = false ) {
+		return self::GetImage($link, true);
+	}
+
+	// Gets Live Image URL
+	public function GetImage( $link, $all = false ) {
+		if(is_object($link)) {
+			if(!empty($link->images->url)) {
+				$link = $link->images->url;
+			} else {
+				return false;
+			}
+		}
+		$image = Bigcommerce_api::GetDetail( $link );
+		if(!$image) { return false; }
+		$images = self::JSONToObject( $image );
+		if(!$images) { return false; }
+		foreach((array)$images as $image) {
+			if(!empty($image->is_thumbnail) && empty($all)) {
+				return self::storepath() . 'product_images/' . (string)$image->image_file;
+			}
+			$output[] = self::storepath() . 'product_images/' . (string)$image->image_file;
+		}
+		if($all) { return $output; }
+	}
+
+
+	// Builds Select Box For Products
+	public function BuildProductsSelect( $rebuild, $show_output = false) {
+
+		// Not Forcing Rebuild
+		if( ! $rebuild ) {
+			return get_option( 'wpinterspire_productselect' );
+		}
+
+		// Get Products
+		$items = Bigcommerce_api::GetProducts( empty($rebuild), $show_output);
+
+		if ( empty($items) ) { return false; }
+
+		// Generate HTML Selector
+		$output = '
+			<select id="interspire_add_product_id">
+			<option value="" disabled="disabled" selected="selected">Products</option>
+		';
+
+		$hide_invisible = Bigcommerce_settings::get_option('hideinvisible');
+		#echo '<pre>';
+		#print_r($items);
+		#echo '</pre>';
+		#die();
+		foreach( $items as $item ) {
+			if( ! isset( $item->name ) ) { continue; }
+			$value = $item->custom_url;
+			$name = esc_html( $item->name );
+			$hidden = '';
+			if( ! isset( $item->is_visible ) || ! $item->is_visible) {
+				if($hide_invisible) { continue; }
+				$hidden = __(' (Not Visible in Store)', 'wpinterspire');
+			}
+			$output .= "<option value='{$value}'>{$name}{$hidden}</option>";
+		}
+		$output .= '</select>';
+
+		// Save HTML Selector To Cache
+		// Delete first and add so it can specify no autoload.
+		delete_option( 'wpinterspire_productselect' );
+		add_option( 'wpinterspire_productselect', $output, '', 'no' );
+
+		return $output;
+	}
+
+	// Builds Select Box For Categories
+	public function BuildCategoriesSelect( $rebuild, $show_output = false ) {
+
+		// Not Forcing Rebuild
+		if( ! $rebuild ) {
+			return get_option( 'wpinterspire_categoryselect' );
+		}
+
+		// Get Products
+		if ( ! $items = Bigcommerce_api::GetCategories($rebuild, $show_output) ) { return false; }
+
+		// Generate HTML Selector
+		$output = '
+			<select id="interspire_add_category_id">
+			<option value="" disabled="disabled" selected="selected">Categories</option>
+		';
+		$categories = array();
+		$options = array();
+
+		// Build all cats for reference
+		$categories = array();
+		foreach( $items as $item ) {
+			$name = esc_html( $item->name );
+			$categories[(int) $item->id] = esc_html( $name );
+		}
+
+		foreach( $items as $item ) {
+			$name = esc_html( $item->name );
+
+			if( empty($item->is_visible) ) { continue; }
+			if( ! isset( $item->name ) ) { continue; }
+			$value = $item->id;
+
+			foreach( $item->parent_category_list as $val ) {
+				if(empty($val)) { continue; }
+				$parent = $categories[(int) $val];
+				if( empty($parent) || $parent == $name ) { continue; }
+				$name = "{$parent} &gt; {$name}";
+			}
+			$options[$name] = "<option value='{$value}'>{$name}</option>";
+		}
+		ksort( $options );
+		foreach( $options as $option ) { $output .= $option; }
+		$output .= '</select>';
+
+		// Save HTML Selector To Cache
+		// Delete first and add so it can specify no autoload.
+		delete_option( 'wpinterspire_categoryselect' );
+		add_option( 'wpinterspire_categoryselect', $output, '', 'no' );
+
+		return $output;
+	}
+
+	// Outputs Products In a Category
+	function DisplayProductsInCategory( $catid ) {
+		$output = '';
+
+		// Find Products
+		$products = Bigcommerce_api::GetProducts();
+
+		if( $products ) {
+
+			foreach( $products as $product ) {
+				foreach( $product->categories as $product_category ) {
+
+					$product_category = isset($product_category->value) ? intval( $product_category->value ) : intval($product_category);
+
+					// Product Matches Category
+					if( (int)$catid === $product_category ) {
+
+						// Ensure Visible
+						if( $product->is_visible != 'true' && $product->is_visible != '1' ) { continue; }
+
+						// Check For Image
+						$image = self::GetImage( $product );
+						if(empty($image)) {
+							$image = '<p>No image available</p>';
+						}
+
+						// Output The Row
+						$output .= Bigcommerce_display::DisplayProductRow(
+							(object) array(
+								'is_featured' => (
+									( (string) $product->is_featured == 'true' )
+										? 'featured' : ''
+								),
+								'name' => (string) $product->name,
+								'sku' => (
+									( (string) $product->sku )
+										? (string) $product->sku
+										: 'Not specified'
+								),
+								'price' => (
+									( (string) $product->is_price_hidden == 'true' )
+										? 'Not specified'
+										: (
+											( (int) $product->retail_price > 0 )
+											? '$' . number_format( (int) $product->retail_price, 2 )
+											: '$' . number_format( (int) $product->price, 2 )
+										)
+								),
+								'condition' => (
+									( (string) $product->is_condition_shown == 'false' )
+										? 'Not specified'
+										: (string) $product->condition
+								),
+								'availability' => ucwords( (string) $product->availability ),
+								'link' => sanitize_title( (string) $product->name ),
+								'image' => $image,
+								'warranty' => (
+									( (string) $product->warranty )
+										? (string) $product->warranty
+										: 'Not specified'
+								),
+								'rating' => (
+									( (int) $product->rating_count === 0 )
+										? 'No ratings available'
+										: (int) $product->rating_total
+											. " (from {$data->rating_count} ratings)"
+								),
+							)
+						);
+					}
+				}
+			}
+		}
+
+		// Output
+		return ( $output )
+			? $output
+			: __(
+				sprintf( "Unable to find any products within category ID: %d</p>", $catid ),
+				'wpinterspire'
+			);
+	}
+}
+
+?>
